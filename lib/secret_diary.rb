7dabc24477754aa68368d7340685e32aa123c936
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-05-05 20:35:33 +0100

    Raise error when adding to locked diary

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,8 +1,11 @@
 class SecretDiary
   attr_reader :locked
-  
+
   def initialize
     @locked = true
   end
 
+  def add_entry(entry)
+    raise "Cannot add content to a locked diary" if @locked
+  end
 end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -5,7 +5,20 @@ describe 'SecretDiary' do
     @secret_diary = SecretDiary.new
   end
 
-  it 'creates a locked diary' do
-    expect(@secret_diary.locked).to eq(true)
+  describe '#initialize' do
+    it 'creates a locked diary' do
+      expect(@secret_diary.locked).to eq(true)
+    end
+  end
+
+  describe '#add_entry' do
+    it 'requires an argument' do
+      expect(@secret_diary).to respond_to(:add_entry).with(1).argument
+    end
+
+    it 'raises error if try to add content to locked diary' do
+      expect { @secret_diary.add_entry("Hello") }.to \
+      raise_error("Cannot add content to a locked diary")
+    end
   end
 end
